1dca04ad44fd615d6d5ea8a9d958a2202348e412
Sampath Kumara <sampathk@lbfinance.lk>
2023-07-15 10:51:20 +0530

    Add project files.

---
+++ CredentialInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCachingApplication
+{
+    internal record CredentialInfo
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using CachingManager;
+using SampleCachingApplication;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+InMemoryCacheRepository<object> cache = new CachingManager.InMemoryCacheRepository<object>();
+CredentialInfo credentialInfo = new CredentialInfo();
+credentialInfo.Name = "Sampath  Kumara";
+cache.Add("Credentials", credentialInfo);
